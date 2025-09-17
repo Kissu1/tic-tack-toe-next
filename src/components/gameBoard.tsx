6f391771d199b080d737e14d42b94cf45f74a45f
Kissu1 <ricoposas@gmail.com>
2025-09-17 23:05:27 +0800

    Add win condition
    -add helper util

--- src/components/gameBoard.tsx
+++ src/components/gameBoard.tsx
@@ -1,18 +1,25 @@
 import Square from './Square';
-import type { BoardProps, BoardState } from '../types';
+import type { BoardProps } from '../types';
+import { calculateWinner } from '../utils/calculateWinner';
 
 export default function Board({ squares, xIsNext, onPlay }: BoardProps) {
-  function handleClick(i: number) {
-    if (squares[i]) return;
+  const winner = calculateWinner(squares);
 
-    const nextSquares: BoardState = squares.slice();
+  function handleClick(i: number) {
+    if (squares[i] || winner) return;
+
+    const nextSquares = squares.slice();
     nextSquares[i] = xIsNext ? 'X' : 'O';
     onPlay(nextSquares);
   }
 
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? 'X' : 'O'}`;
+
   return (
     <div className="flex flex-col gap-2 items-center">
-      <div className="font-semibold">Next player: {xIsNext ? 'X' : 'O'}</div>
+      <div className="font-semibold">{status}</div>
       <div className="grid grid-cols-3 gap-2">
         {squares.map((value, idx) => (
           <Square
